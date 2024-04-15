7e9e815fa423c77acd98c87b3c5e8a50481b7093
Jocke <joakim03moller@gmail.com>
2024-04-15 16:48:10 +0200

    Lagt till komponenter och moduler. Navigering finns

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ValueConversionComponent } from './value-conversion/value-conversion.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'value-conversion', component: ValueConversionComponent},
+    { path: 'navigation', component: NavigationComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '404', component: NotFoundComponent},
+    { path: '**', component: NotFoundComponent}
+];
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    // Hämta in meny-knapparna
+    let openBtn = document.getElementById("open-menu") as HTMLButtonElement;
+    let closeBtn = document.getElementById("close-menu") as HTMLButtonElement;
+
+    // Eventlyssnare
+    openBtn.addEventListener('click', this.toggleMenu);
+    closeBtn.addEventListener('click', this.toggleMenu);
+  }
+
+  toggleMenu() {
+    let navMenuEl = document.getElementById("nav-menu") as HTMLElement;
+
+    // Hämtar in css för menyn
+    let style = window.getComputedStyle(navMenuEl);
+
+    // Koll om navigering är synlig eller ej, ändrar display block/none
+    if (style.display === "none") {
+      navMenuEl.style.display = "block";
+    } else {
+      navMenuEl.style.display = "none";
+    }
+  }
 
 }
