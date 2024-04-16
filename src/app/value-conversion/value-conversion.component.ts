d8c8071ff861ef71381b4c9979a601edb8a0ec90
Jocke <joakim03moller@gmail.com>
2024-04-16 10:25:42 +0200

    Fungerande konvertering för meter/fot

--- src/app/value-conversion/value-conversion.component.ts
+++ src/app/value-conversion/value-conversion.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-value-conversion',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './value-conversion.component.html',
   styleUrl: './value-conversion.component.scss'
 })
-export class ValueConversionComponent {
+export class ValueConversionComponent { 
+  meterValue: number = 0;
+  footValue: number = 0;
 
+  convertMeterToFoot() {
+    this.footValue = this.meterValue * 3.28084;
+  }
+
+  convertFootToMeter() {
+    this.meterValue = this.footValue / 3.28084;
+  }
 }
